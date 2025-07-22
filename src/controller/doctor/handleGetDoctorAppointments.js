df1e776f90e95a7a69430891bdcfc449e91ec5a6
ayham <[ayham10.aboajeb@gmail.com]>
2025-07-22 17:01:49 +0300

    fix

--- src/controller/doctor/handleGetDoctorAppointments.js
+++ src/controller/doctor/handleGetDoctorAppointments.js
@@ -2,14 +2,18 @@ const { startOfDay, endOfDay } = require('date-fns');
 const appointmentModel = require('../../models/appointmentModel');
 const AppError = require('../../utils/AppError');
 const ObjectId = require('mongoose').Types.ObjectId;
+const { formatInTimeZone, getTimezoneOffset, toZonedTime, format, fromZonedTime } = require('date-fns-tz')
 
 const handleGetDoctorAppointments = async (req, res, next) => {
     const doctorId = res.locals.id;
     const date = req.query.date;
     try {
         if (!date) { throw new AppError(404, 'no specific date'); }
+        const startUTC = toZonedTime(startOfDay(date));
+        const endUTC = toZonedTime(endOfDay(date));
         const response = await appointmentModel.aggregate([
-            { $match: { doctorId: new ObjectId(doctorId), date: { $gte: startOfDay(date), $lte: endOfDay(date) } } },
+            { $match: { doctorId: new ObjectId(doctorId), date: { $gte: startUTC, $lte: endUTC } } },
+            // { $match: { doctorId: new ObjectId(doctorId), date: { $gte: startOfDay(date), $lte: endOfDay(date) } } },
             {
                 $lookup: {
                     from: 'users',
